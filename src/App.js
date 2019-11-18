405f8569c47837bf9ca308709f5a36344b4e5172
ajmerr13 <oliverclosoff07@gmail.com>
2019-11-18 17:18:17 -0500

    app update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Login from './components/Login'
+import Register from './components/Register'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello World!</h1>
+      <header>
+        <Login />
+        <Register />
       </header>
     </div>
   );
