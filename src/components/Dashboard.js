b88c0ae1424042b3261b1e928c6de51dd525bfeb
Zac Higgins <zachiggins@Higgins-iMac.local>
2019-11-19 12:08:18 -0700

    fixed the response data path on the suggestedlist. added suggested list to dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,11 +3,13 @@ import SavedList from './SavedList';
 import PostForm from './PostForm';
 import SearchForm from './SearchForm';
 import ResultCard from './ResultCard';
+import SuggestedList from "./SuggestedList";
 
 const Dashboard = () => {
     return(
         <div className="dashboard">
             <PostForm />
+            <SuggestedList />
         </div>
     )
 }
