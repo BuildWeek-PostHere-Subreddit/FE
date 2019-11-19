412dbb3c02bb2e739ce9d3c577baf7473917321d
Zac Higgins <zachiggins@Higgins-iMac.local>
2019-11-18 17:22:51 -0700

    Built out login and register forms. Set up Redux. Set up private routes. Built basics for dashboard component.
    Co-authored-by: AJ ajmerr13@gmail.com

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import './App.css';
 import Login from './components/Login'
 import Register from './components/Register'
+import Dashboard from './components/Dashboard';
+import PrivateRoute from './components/PrivateRoute';
+
 
 function App() {
   return (
     <div className="App">
-      <header>
-        <Login />
-        <Register />
-      </header>
+      <h1>Hello World!</h1>
+      <Router>
+        <Switch>
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+          <PrivateRoute path="/">
+            <Dashboard />
+          </PrivateRoute>
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,11 +1,36 @@
 import React, {useState, useEffect} from "react";
 import {withFormik, Form, Field} from "formik";
 import axios from "axios";
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+
+
+const useStyles = makeStyles(theme => ({
+    text: {
+      fontSize: 20,
+      marginRight: 86,
+    },
+    button: {
+        fontSize: 30,
+      },
+    card: {
+        maxWidth: 500,
+        background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    },
+    h1:{
+        fontFamily: 'Raleway',
+    }
+  }));
+  
 
 const Register = ({status}) => {
 
     const [user, setUser] = useState([]);
-
+    const classes = useStyles();
     useEffect(() =>{
         if (status) {
             setUser([...user, status])
@@ -15,20 +40,26 @@ const Register = ({status}) => {
    
     return(
         <div className="registerForm">
-            <h1>Welcome New User, Register Here</h1>
-            <Form>
-                <Field type="username" name="username" placeholder="username" />
+            <h1 className={classes.h1}>Welcome New User, Register Here</h1>
+            <Card className={classes.card}>
                 
-                <Field type="password" name="password" placeholder="password" />
-                           
-                <button type="submit">Register</button>  
-            </Form>
-            {user.map(person => (
-                <ul key={person.id}>
-                    <li>Username: {person.username}</li>
-                    <li>Password: {"●".repeat(person.password.length)}</li>
-                </ul>
-            ))}
+                    <h2>Welcome Back, Returning User</h2>
+                    
+                        <Form>
+                            <Typography className={classes.text} >
+                                Username:
+                             </Typography >
+                            <Field type="username" name="username" placeholder="username" />
+                            </Form>
+                            <Form>
+                            <Typography className={classes.text} >
+                                Password:
+                                </Typography>
+                            <Field type="password" name="password" placeholder="password" />
+                            <br /><br />
+                            <Button className={classes.button} size="large" type="submit">Register</Button>
+                        </Form>
+            </Card>
             </div>
 
     )}
@@ -42,8 +73,9 @@ const FormikRegister = withFormik({
     },
 
     handleSubmit(values, {setStatus}){
+        console.log(values);
         axios
-            .post("https://reqres.in/api/users/", values)
+            .post("https://backend-posthere-russ-and-mack.herokuapp.com/users/register", values)
             .then(response =>{
                 console.log(response);
                 setStatus(response.data);
