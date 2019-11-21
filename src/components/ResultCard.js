e0390ae3dc66ce60eb24e0d53c52074c3dbb8194
Zac Higgins <zachiggins@Higgins-iMac.local>
2019-11-21 11:26:20 -0700

    troubleshooting backend put request issue

--- src/components/ResultCard.js
+++ src/components/ResultCard.js
@@ -13,7 +13,7 @@ export function ResultCard(props) {
     const backendSubmit = event => {
         event.preventDefault();
         //user_id is hard coded to 6 right now. Need to fix a redux store issue where the user id is not being updated with the one given upon login
-        const resultInfo = { user_id: 6, title: props.post.title, text: props.post.text, link: props.post.link, first: props.results[0].name, first_subscribers: props.results[0].subscribers, first_description: props.results[0].description, first_url: props.results[0].url, second: props.results[1].name, second_subscribers: props.results[1].subscribers, second_description: props.results[1].description, second_url: props.results[1].url, third: props.results[2].name, third_subscribers: props.results[2].subscribers, third_description: props.results[2].description, third_url: props.results[2].url }
+        const resultInfo = { user_id: 1, title: props.post.title, content: props.post.text, link: props.post.link, first: props.results[0].name, first_subscribers: props.results[0].subscribers, first_description: props.results[0].description, first_url: props.results[0].url, second: props.results[1].name, second_subscribers: props.results[1].subscribers, second_description: props.results[1].description, second_url: props.results[1].url, third: props.results[2].name, third_subscribers: props.results[2].subscribers, third_description: props.results[2].description, third_url: props.results[2].url }
         console.log("backend submit", resultInfo)
         props.saveResults(resultInfo)
     }
