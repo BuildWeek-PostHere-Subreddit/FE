8110eccd100ea8683907b544611897f204e29464
ajmerr13 <oliverclosoff07@gmail.com>
2019-11-21 17:10:42 -0500

    nav bar "styled"

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,23 +1,54 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
 import { withRouter } from 'react-router-dom';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+
+const useStyles = makeStyles(theme => ({
+  
+   
+    card: {
+        maxWidth: 500,
+        background: '#2E3B55',
+        borderRadius: 0,
+        height: 20,
+        spacing: 12
+    },
+    link: {
+       color: '#FFFFFF',
+       fontSize: 20
+    },
+
+}));
 
 function NavBar(props) {
+
+    const classes = useStyles();
     if (!localStorage.getItem("token")) {
         return (
-            <div className="nav">
-                <Link to="/login">Login</Link>
-                <Link to="/register">Register</Link>
-            </div>
+
+            <Card className={classes.card}>
+                <Link className={classes.link} to="/login">Login</Link>
+                "                                                     "
+                <Link className={classes.link} to="/register">Register</Link>
+            </Card>
         );
     }
     else {
         return (
-            <div className="nav">
-                <Link to="/">Home</Link>
-                <Link to="/saved">Saved Results</Link>
-                <Link onClick={() => { console.log("Deleting token and logging out"); localStorage.removeItem("token"); props.history.push("/login"); }}>Log Out</Link>
-            </div>
+            <Card className={classes.card}>
+                <div>
+                <Link className={classes.link} to="/">Home</Link>
+                "                         "
+                <Link className={classes.link} to="/saved">Saved Results</Link>
+                "                         "
+                <Link className={classes.link} onClick={() => { console.log("Deleting token and logging out"); localStorage.removeItem("token"); props.history.push("/login"); }}>Log Out</Link>
+                </div>
+            </Card>
         );
     }
 }
